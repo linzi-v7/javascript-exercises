9833c4ea301c225e7dfddcd7d9a101b3ef1cfb5a
linzi-v7 <farisos7777@gmail.com>
2024-10-07 17:04:28 +0300

    Passed exercise's tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,22 @@
-const reverseString = function() {
+const reverseString = function (text)
+{
 
+    if (text === undefined)
+    {
+        return "ERROR";
+    }
+
+    let textLengthIndex = text.length - 1; //last character index
+
+    let reversedString = "";
+
+    while (textLengthIndex >= 0)
+    {
+        reversedString += text[textLengthIndex];
+        textLengthIndex--;
+    }
+
+    return reversedString;
 };
 
 // Do not edit below this line
