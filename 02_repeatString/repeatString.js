e0d0fbf4798115a5a9c5bbeca94e064c93d621be
linzi-v7 <farisos7777@gmail.com>
2024-10-07 16:49:51 +0300

    Passed excercise's tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (text, times)
+{
+    if (times < 0)
+    {
+        return "ERROR";
+    }
 
+    let repeatedString = "";
+
+    for (let index = 0; index < times; index++)
+    {
+        repeatedString += text;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
