0b8d23bb966f1bf40edde641f7d15c3f3a8eca24
linzi-v7 <farisos7777@gmail.com>
2024-10-08 08:17:01 +0300

    3 tests passed, rest later

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, position, secondPosition = -1)
+{
+    if (position - 1 < array.length)
+    {
+        array.splice(position - 1, 1);
+
+        if (secondPosition != -1)
+        {
+            array.splice(secondPosition - 1, 1);
+        }
+
+    }
+
+    return array;
 };
 
 // Do not edit below this line
