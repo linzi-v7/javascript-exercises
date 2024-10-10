a353f59d0ec03caeebf3ccf7d4560bb9871c6b43
linzi-v7 <farisos7777@gmail.com>
2024-10-10 08:10:42 +0300

    All Tests Passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,30 @@
-const sumAll = function() {
+const sumAll = function (beginning, ending)
+{
+    if ((beginning < 0) || (ending < 0) || !(Number.isInteger(beginning)) || !(Number.isInteger(ending)))
+    {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    if (beginning < ending)
+    {
+        for (let index = beginning; index <= ending; index++)
+        {
+            sum += index;
+        }
+    }
+    else 
+    {
+        for (let index = beginning; index >= ending; index--)
+        {
+            sum += index;
+        }
+    }
+
+    return sum;
 };
+
+console.log(sumAll(123, 1))
 
 // Do not edit below this line
 module.exports = sumAll;
