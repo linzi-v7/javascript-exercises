333a3afef417ab4a2a18d02b9f5a458363adb77b
linzi-v7 <farisos7777@gmail.com>
2024-11-03 06:39:04 +0200

    Passed test exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (text)
+{
+    text = text.toLowerCase();
+    text = text.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '') //remove punctuation
+    text = text.replace(/\s/g, '') //remove whitespace
+    //text.replace(/\W/g, '');
+    //return text;
+
+    reversed = text.split("").reverse().join("")
+
+    if (reversed == text)
+    {
+        return true
+    }
+
+    return false;
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
