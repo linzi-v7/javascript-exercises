483d2a639ac8c54f12117c0d4b05f4612ebcf753
linzi-v7 <farisos7777@gmail.com>
2024-11-04 09:55:52 +0200

    Pass test exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function (num)
+{
+    if (num < 0)
+    {
+        return "OOPS";
+    }
+    else if (num == 0)
+    {
+        return 0;
+    }
 
+    arr = [1, 1]
+
+    for (i = 2; i <= num - 1; i++)
+    {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    return arr[num - 1];
 };
 
+
+fibonacci(2);
 // Do not edit below this line
 module.exports = fibonacci;
